evt-project/evt-viewer-angular

Change layer separator from arrow to '>', add config option in edition-config.json, reverse changes order in mod group heading

src/app/services/evt-status.service.ts
@@ -206,7 +206,7 @@ export class EVTStatusService {
 
     /** to avoid loops this function must not be fed with nodes */
     getPageElementsByClassList(classList) {
-        const notInterestingProps = ['originalEncoding','type','spanElements'];
+        const notInterestingProps = ['originalEncoding','type','spanElements', 'includedElements'];
         const maxEffort = 4000;
 
         return this.currentStatus$.pipe(

pax3l/evt-viewer-angular-ovid

src/app/services/evt-status.service.ts
@@ -208,7 +208,7 @@ export class EVTStatusService {
 
     /** to avoid loops this function must not be fed with nodes */
     getPageElementsByClassList(classList) {
-        const attributesNotIncludedInSearch = ['originalEncoding','type','spanElements'];
+        const attributesNotIncludedInSearch = ['originalEncoding','type','spanElements','includedElements'];
         const maxEffort = 4000;
 
         return this.currentStatus$.pipe(