evt-project/evt-viewer-angular

Prevent loops inside deepSearch utils when fed with a node list

src/app/services/evt-status.service.ts
@@ -204,8 +204,9 @@ export class EVTStatusService {
         };
     }
 
+    /** to avoid loops this function must not be fed with nodes */
     getPageElementsByClassList(classList) {
-        const notInterestingProps = ['originalEncoding','type'];
+        const notInterestingProps = ['originalEncoding','type','spanElements'];
         const maxEffort = 4000;
 
         return this.currentStatus$.pipe(

pax3l/evt-viewer-angular-ovid

src/app/services/evt-status.service.ts
@@ -206,8 +206,9 @@ export class EVTStatusService {
         };
     }
 
+    /** to avoid loops this function must not be fed with nodes */
     getPageElementsByClassList(classList) {
-        const attributesNotIncludedInSearch = ['originalEncoding','type'];
+        const attributesNotIncludedInSearch = ['originalEncoding','type','spanElements'];
         const maxEffort = 4000;
 
         return this.currentStatus$.pipe(