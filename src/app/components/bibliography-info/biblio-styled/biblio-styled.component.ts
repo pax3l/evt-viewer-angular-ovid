a18a0fe15ff797d64c4508b8ace8bdb58ea1be11
Lorenzo Bafunno <bfn.lorenzo@gmail.com>
2024-04-30 15:46:15 +0200

    Add BiblStructEntry handling in StyledBiblio component

--- src/app/components/bibliography-info/biblio-styled/biblio-styled.component.ts
+++ src/app/components/bibliography-info/biblio-styled/biblio-styled.component.ts
@@ -1,31 +1,113 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { AppConfig } from 'src/app/app.config';
-import { BibliographicEntry } from 'src/app/models/evt-models';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AfterViewInit, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges, TemplateRef, ViewChild } from '@angular/core';
+import { AppConfig, BibliographicStyle, CitingRanges } from 'src/app/app.config';
+import { BibliographicEntry, BibliographicStructEntry } from 'src/app/models/evt-models';
 
 @Component({
   selector: 'evt-styled-biblio-entry',
   templateUrl: './biblio-styled.component.html',
   styleUrls: ['./biblio-styled.component.scss'],
 })
-export class StyledBiblioEntryComponent implements OnChanges{
-  @Input() data: BibliographicEntry;
+export class StyledBiblioEntryComponent implements OnChanges, AfterViewInit {
+
+  @ViewChild('title', { static: false }) title: TemplateRef<any>;
+  @ViewChild('author', { static: false }) author: TemplateRef<any>;
+  @ViewChild('publication', { static: false }) publication: TemplateRef<any>;
+  @ViewChild('editor', { static: false }) editor: TemplateRef<any>;
+  @ViewChild('date', { static: false }) date: TemplateRef<any>;
+  @ViewChild('pubPlace', { static: false }) pubPlace: TemplateRef<any>;
+  @ViewChild('publisher', { static: false }) publisher: TemplateRef<any>;
+  @ViewChild('doi', { static: false }) doi: TemplateRef<any>;
+
+
+  @Input() data: BibliographicEntry | BibliographicStructEntry;
   @Input() style: string = AppConfig.evtSettings.ui.defaultBibliographicStyle;
 
-  public showList;
+  public biblEntry;
+  public showList: string[];
   public showAttrNames = AppConfig.evtSettings.edition.biblView.showAttrNames;
   public showEmptyValues = AppConfig.evtSettings.edition.biblView.showEmptyValues;
   public inline = AppConfig.evtSettings.edition.biblView.inline;
   public isCommaSeparated = AppConfig.evtSettings.edition.biblView.commaSeparated;
   public showMainElemTextContent = AppConfig.evtSettings.edition.biblView.showMainElemTextContent;
-  public styleProperties;
+  public styleProperties : BibliographicStyle;
+
+  flattenBiblStruct(entry: BibliographicStructEntry): BibliographicEntry[] {
+    return entry.analytic.concat(entry.monogrs.concat(entry.series));
+  }
+
+  getContextForElement(element: string, structEntry: BibliographicStructEntry): BibliographicEntry[]{
+    let context;
+    switch(element){
+      case 'title':
+        context = structEntry.analytic;
+        break;
+      case 'publication':
+      case 'date':
+      case 'publPlace':
+      case 'publisher':
+      case 'editor':
+        context = structEntry.monogrs;
+        break;
+      default:
+        context = this.flattenBiblStruct(structEntry);
+        break;
+    }
+
+    return context;
+  }
+
+  firstContainsProperty(entries: BibliographicEntry[], element:string){
+    const elementInFirstEntry = entries[0]?.[element];
+
+    return elementInFirstEntry && elementInFirstEntry.length > 0;
+  }
+
+  containsOnlyEmptyValues(arr: string[]){
+    return arr.reduce((prev, x) => (x === '') && prev, true);
+  }
+
+  requiresAcronym(elem: CitingRanges){
+    const publicationStyle = this.styleProperties.publicationStyle || { citingAcronym: 'none' };
+    if(!publicationStyle?.citingAcronym) { return false; }
+
+    if(publicationStyle.citingAcronym === 'all'){
+      return true;
+    }else if(publicationStyle.citingAcronym === 'none'){
+      return false;
+    }
+
+    return publicationStyle.citingAcronym.includes(elem);
+  }
+
+  getPublisherDetailsOrder(){
+    return this.showList.filter((x) => x === 'publisher' || x === 'pubPlace');
+  }
+
+  getAuthorsDetails(entries: BibliographicEntry[]){
+    return entries.reduce((prev, e) => prev.concat(e.authorsDetails), []);
+  }
+
+  isStructured(entry: BibliographicEntry){
+    // searching for the most relevant signs of a structured entry.
+    return entry.originalEncoding.querySelectorAll('title, author, date').length > 0;
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
+    if(this.data.type && this.data.type === BibliographicEntry){
+      this.biblEntry = this.data as BibliographicEntry;
+    }
+    if(this.data.type && this.data.type === BibliographicStructEntry){
+      this.biblEntry = this.data as BibliographicStructEntry;
+    }
     if(changes.style){
       this.showList = AppConfig.evtSettings.ui.allowedBibliographicStyles[this.style].propsOrder;
       this.styleProperties = AppConfig.evtSettings.ui.allowedBibliographicStyles[this.style].properties;
     }
   }
 
-
+  constructor(private cd: ChangeDetectorRef){}
+  ngAfterViewInit(): void {
+    this.cd.detectChanges();
+  }
 }
-
